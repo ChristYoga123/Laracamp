d06d9a49b0b807511223dfe056932d28ebe777ce
ChrisYoga123 <christianuswibisono@gmail.com>
2022-05-02 01:51:06 +0700

    slicing login, dashboard, checkout, success-checkout

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/login', function () {
+    return view('login');
+})-> name('login'); // name('login') is used to redirect to the login page
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})-> name('checkout'); // name('checkout') is used to redirect to the checkout page
+
+Route::get('/success-checkout', function () {
+    return view('success_checkout');
+})-> name('success-checkout'); // name('success-checkout') is used to redirect to the contact page
